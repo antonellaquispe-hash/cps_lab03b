770b4b47edc0497048aaaf0797b6bbebf4beffb6
antonellaquispe-hash <antonella.quispe@tecsup.edu.pe>
2026-09-01 17:15:01 -0500

    Modificacion

--- src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
+++ src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
@@ -1,4 +1,64 @@
 package pe.edu.tecsup.lab03.entities;
 
 public class StudentEntity {
+
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    // Constructor sin argumentos
+    public StudentEntity() {
+    }
+
+    // Constructor con argumentos
+    public StudentEntity(int id, String nombre, String apellido, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentEntity{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
